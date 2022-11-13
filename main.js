86c3d56ccf49c02bcb25247472122f6b8f129830
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-11-13 11:45:44 -0800

    Feat: add Navbar for mobile

---
+++ main.js
@@ -0,0 +1,7 @@
+function showNav() {
+  document.getElementById("hamburger-nav-menu").classList.remove("hide-nav");
+}
+
+function hideNav() {
+  document.getElementById("hamburger-nav-menu").classList.add("hide-nav");
+}
