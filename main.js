8afd77b83d2464b50f49900c4c2e3146c5ff0b12
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2022-11-20 19:58:55 -0800

    Desktop view

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function showNav() {
 function hideNav() {
   document.getElementById("hamburger-nav-menu").classList.add("hide-nav");
 }
+
+function showSection(section) {
+  const sections = document.querySelector(".main");
+  Object.keys(sections.children).forEach((item) => {
+    sections.children[item].classList.remove("visible");
+    sections.children[item].classList.add("not-visible");
+  });
+  document.getElementById(section).classList.add("visible");
+  document.getElementById(section).classList.remove("not-visible");
+}
